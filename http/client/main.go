cc6ad821df7a7f3343e59f6cb7797a230eddaf15
kry shitenkova <shitenkovakris@mail.ru>
2023-07-04 13:58:02 +0300

    create http client

---
+++ http/client/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+func main() {
+	//создаем новый http клиент
+	httpClient := &http.Client{}
+
+	//создаем http запрос
+	request, err := http.NewRequest("GET", "http://mystudy.com", nil)
+	if err != nil {
+		fmt.Println(errors.Wrapf(err, "can not create request"))
+
+		return
+	}
+
+	// отправляем запрос с помощью клиента
+	response, err := httpClient.Do(request)
+	if err != nil {
+		fmt.Println(errors.Wrapf(err, "can not send response"))
+
+		return
+	}
+	defer response.Body.Close()
+
+	// читаем тело ответа
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(errors.Wrapf(err, "can not read answer from the server"))
+
+		return
+	}
+
+	// выводим тело ответа
+	fmt.Println(string(body))
+}
